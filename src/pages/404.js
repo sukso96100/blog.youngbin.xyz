eb7138c85469b22894ddeeea229b7b6121621187
sukso96100 <sukso96100@gmail.com>
2019-05-23 20:04:55 +0900

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,22 +1,72 @@
 import React from "react"
-import { graphql } from "gatsby"
+import PropTypes from "prop-types"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+// Utilities
+import kebabCase from "lodash/kebabCase"
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+// Components
+import { Link, graphql } from "gatsby"
+import Chip from "@material-ui/core/Chip"
+import Avatar from "@material-ui/core/Avatar"
+import Paper from "@material-ui/core/Paper"
+import Shell from "../components/shell"
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
-    )
-  }
+const NotFoundPage = ({
+  data: {
+    allMarkdownRemark: { group },
+    site: {
+      siteMetadata: { title },
+    },
+  },
+}) => (
+  <Shell title="All Tags">
+    <div
+      style={{
+        margin: 0,
+        width: "100%",
+        height: "100vh",
+        minHeight: 650,
+        background: "black",
+        color: "white",
+      }}
+    >
+      <div
+        style={{
+          marginLeft: "auto",
+          marginRight: "auto",
+          maxWidth: 1000,
+          padding: 16,
+          paddingTop: 64,
+        }}
+      >
+        <h3>{title}</h3>
+        <br />
+        <h1>Ooops! Nothing was found here!</h1>
+        <br />
+        <Link to={'/'} style={{textDecoration: 'none'}}>
+          <Button>Move to blog home</Button>
+        </Link>
+      </div>
+    </div>
+  </Shell>
+)
+
+TagsPage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      group: PropTypes.arrayOf(
+        PropTypes.shape({
+          fieldValue: PropTypes.string.isRequired,
+          totalCount: PropTypes.number.isRequired,
+        }).isRequired
+      ),
+    }),
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+      }),
+    }),
+  }),
 }
 
 export default NotFoundPage
