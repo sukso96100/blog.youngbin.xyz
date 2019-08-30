ad7a30f56a6a91e2102389ab38de35b4dc985946
sukso96100 <sukso96100@gmail.com>
2019-08-30 15:40:12 +0900

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,15 +57,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-46663268-3`,
       },
     },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `YoungbinLab Blog`,
+        short_name: `YoungbinLab`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
