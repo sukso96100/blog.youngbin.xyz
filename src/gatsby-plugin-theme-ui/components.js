d736795b046d5d11de70eeb8f6620168d3865431
sukso96100 <sukso96100@gmail.com>
2020-11-21 16:38:35 +0900

    Add more language support on codeblock hightlighting

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,5 +1,10 @@
 import Prism from '@theme-ui/prism'
+import React from 'react'
+import ThemeUIPrism from '@theme-ui/prism'
+import PrismCore from 'prismjs/components/prism-core'
+import 'prismjs/components/index'
+ 
 export default {
   pre: props => props.children,
-  code: Prism,
+  code: props => <ThemeUIPrism {...props} Prism={PrismCore} />,
 }
