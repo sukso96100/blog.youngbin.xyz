e893923607306aad1cec6532355debd1d811a678
sukso96100 <sukso96100@gmail.com>
2020-11-21 20:04:30 +0900

    Hightlight fixes

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,11 +1,44 @@
 import React from 'react'
 import ThemeUIPrism from '@theme-ui/prism'
 import PrismCore from 'prismjs/components/prism-core'
-import 'prismjs/components/prism-clike'
-import 'prismjs/components/prism-csharp'
 
-
-// loadLanguages();
+const langs = [
+  "markup",
+  "bash",
+  "clike",
+  "c",
+  "cpp",
+  "css",
+  "css-extras",
+  "javascript",
+  "jsx",
+  "js-extras",
+  "coffeescript",
+  "diff",
+  "git",
+  "go",
+  "graphql",
+  "handlebars",
+  "less",
+  "makefile",
+  "markdown",
+  "objectivec",
+  "ocaml",
+  "python",
+  "reason",
+  "sass",
+  "scss",
+  "sql",
+  "stylus",
+  "tsx",
+  "typescript",
+  "wasm",
+  "yaml",
+  "csharp",
+  "java"]
+langs.forEach(lang => {
+  require(`prismjs/components/prism-${lang}`)
+})
 export default {
   pre: props => props.children,
   code: props => <ThemeUIPrism {...props} Prism={PrismCore} />,
