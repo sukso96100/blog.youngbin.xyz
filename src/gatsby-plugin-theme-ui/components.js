cd68dcc69d3fb2f8809c1bea3cabd86cde729004
sukso96100 <sukso96100@gmail.com>
2020-11-21 18:27:38 +0900

    Add csharp hightlighting

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ThemeUIPrism from '@theme-ui/prism'
 import PrismCore from 'prismjs/components/prism-core'
-import 'prismjs/components/index'
- 
+import 'prismjs/components/prism-clike'
+import 'prismjs/components/prism-csharp'
+
+
+// loadLanguages();
 export default {
   pre: props => props.children,
   code: props => <ThemeUIPrism {...props} Prism={PrismCore} />,
